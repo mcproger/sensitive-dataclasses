87dd3e2ab8b5efd4726bc4bdb42f3cecfe30b178
mcproger <mcproger7@gmail.com>
2022-06-26 10:17:04 +0300

    Basic tests

--- tests/test_sensitive_dataclasses.py
+++ tests/test_sensitive_dataclasses.py
@@ -1,48 +1,100 @@
+import dataclasses
+
 import pytest
 
-
-def test_repr_sensitive_dataclass(usual_dataclass):
-    result = repr(usual_dataclass)
-
-    assert result == "UsualDataclass(username='test', phone='88005553535')"
-    assert usual_dataclass.password == 'secret'
-    assert usual_dataclass.email == 'sensitive@info.com'
+from sensitive_dataclasses import sensitive_dataclass, SensitiveData
 
 
-def test_class_with_explicit_defined_fields(dataclass_with_explicitly_defined_fields):
-    result = repr(dataclass_with_explicitly_defined_fields)
-
-    assert result == "DataclassWithExplicitlyDefinedFields(username='test')"
-    assert dataclass_with_explicitly_defined_fields.password == 'default-password'
-
-
-def test_frozen_dataclass_respectets_sensitive_type(frozen_dataclass):
-    result = repr(frozen_dataclass)
-
-    assert result == "FrozenDataclass(username='test')"
-    assert frozen_dataclass.password == 'top-secret'
+@sensitive_dataclass
+class UsualDataclass:
+    username: str
+    password: SensitiveData
+    phone: str
+    email: SensitiveData
 
 
-@pytest.mark.skip
-def test_str_sensitive_dataclass():
-    pass
+@sensitive_dataclass
+class DataclassWithExplicitlyDefinedFields:
+    username: str
+    phone: str
+    email: SensitiveData
+    password: SensitiveData = dataclasses.field(
+        default='default-password',
+    )
+    
+
+@sensitive_dataclass(frozen=True)
+class FrozenDataclass:
+    username: str
+    password: SensitiveData
+    phone: str
+    email: SensitiveData
 
 
-@pytest.mark.skip
-def test_sensitive_dataclass_traceback():
-    pass
+@sensitive_dataclass
+class InheritedDataclass(UsualDataclass):
+    bank_card: SensitiveData
+    comment: str
 
 
-@pytest.mark.skip
-def test_log_sensitive_dataclass():
-    pass
+def get_parametrization():
+    return [
+        (
+            UsualDataclass(
+                username='test',
+                password='secret',
+                phone='88005553535',
+                email='sensitive@info.com',
+            ),
+            "UsualDataclass(username='test', phone='88005553535')",
+        ),
+        (
+            DataclassWithExplicitlyDefinedFields(
+                username='test',
+                password='secret',
+                phone='88005553535',
+                email='sensitive@info.com',
+            ),
+            "DataclassWithExplicitlyDefinedFields(username='test', phone='88005553535')",
+        ),
+        (
+            FrozenDataclass(
+                username='test',
+                password='secret',
+                phone='88005553535',
+                email='sensitive@info.com',
+            ),
+            "FrozenDataclass(username='test', phone='88005553535')",
+        ),
+        (
+            InheritedDataclass(
+                username='test',
+                password='secret',
+                phone='88005553535',
+                email='sensitive@info.com',
+                bank_card='bank_card_info',
+                comment='test comment',
+            ),
+            "InheritedDataclass(username='test', phone='88005553535', comment='test comment')",
+        ),
+    ]
 
 
-@pytest.mark.skip
-def test_class_inheritance():
-    pass
+@pytest.mark.parametrize(
+    ('dataclass', 'expected_repr'),
+    get_parametrization()
+)
+def test_repr_with_sensitive_data(dataclass, expected_repr):
+    assert repr(dataclass) == expected_repr
+    assert dataclass.password == 'secret'
+    assert dataclass.email == 'sensitive@info.com'
 
 
-@pytest.mark.skip
-def test_sensitive_dataclass_with_no_annotations():
-    pass
+@pytest.mark.parametrize(
+    ('dataclass', 'expected_str'),
+    get_parametrization()
+)
+def test_str_sensitive_dataclass(dataclass, expected_str):
+    assert str(dataclass) == expected_str
+    assert dataclass.password == 'secret'
+    assert dataclass.email == 'sensitive@info.com'
