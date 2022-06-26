0a4b7d5d8cf720c58828e5eddc8b85b1693e591c
Artem Lomonosov <mcproger7@gmail.com>
2022-06-26 10:37:35 +0300

    Initial implementation (#1)

---
+++ sensitive_dataclasses/sensitive_dataclasses.py
@@ -0,0 +1,48 @@
+import dataclasses
+from typing import Any, NewType, Callable, Optional
+
+SensitiveData = NewType('SensitiveData', Any)
+
+
+def sensitive_dataclass(
+    class_: Optional[type] = None,
+    /,
+    *,
+    init: bool = True,
+    repr: bool = True,
+    eq: bool = True,
+    order: bool = False,
+    unsafe_hash: bool = False,
+    frozen: bool = False,
+) -> Callable:
+    def _process_sensitive_fields(class_: type) -> type:
+        for field, annotation in class_.__annotations__.items():
+            if annotation is not SensitiveData:
+                continue
+
+            explicit_defined_field = getattr(class_, field, None)
+
+            if explicit_defined_field:
+                explicit_defined_field.repr = False
+            else:
+                setattr(class_, field, dataclasses.field(repr=False))
+
+        return class_
+
+    def decorator(class_: type) -> Callable:
+        dataclass_ = dataclasses.dataclass(
+            _process_sensitive_fields(class_),
+            init=init,
+            repr=repr,
+            eq=eq,
+            order=order,
+            unsafe_hash=unsafe_hash,
+            frozen=frozen,
+        )
+
+        return dataclass_
+
+    if class_ is None:
+        return decorator
+
+    return decorator(class_)
---
+++ tests/test_sensitive_dataclasses.py
@@ -0,0 +1,100 @@
+import dataclasses
+
+import pytest
+
+from sensitive_dataclasses import sensitive_dataclass, SensitiveData
+
+
+@sensitive_dataclass
+class UsualDataclass:
+    username: str
+    password: SensitiveData
+    phone: str
+    email: SensitiveData
+
+
+@sensitive_dataclass
+class DataclassWithExplicitlyDefinedFields:
+    username: str
+    phone: str
+    email: SensitiveData
+    password: SensitiveData = dataclasses.field(
+        default='default-password',
+    )
+    
+
+@sensitive_dataclass(frozen=True)
+class FrozenDataclass:
+    username: str
+    password: SensitiveData
+    phone: str
+    email: SensitiveData
+
+
+@sensitive_dataclass
+class InheritedDataclass(UsualDataclass):
+    bank_card: SensitiveData
+    comment: str
+
+
+def get_parametrization():
+    return [
+        (
+            UsualDataclass(
+                username='test',
+                password='secret',
+                phone='88005553535',
+                email='sensitive@info.com',
+            ),
+            "UsualDataclass(username='test', phone='88005553535')",
+        ),
+        (
+            DataclassWithExplicitlyDefinedFields(
+                username='test',
+                password='secret',
+                phone='88005553535',
+                email='sensitive@info.com',
+            ),
+            "DataclassWithExplicitlyDefinedFields(username='test', phone='88005553535')",
+        ),
+        (
+            FrozenDataclass(
+                username='test',
+                password='secret',
+                phone='88005553535',
+                email='sensitive@info.com',
+            ),
+            "FrozenDataclass(username='test', phone='88005553535')",
+        ),
+        (
+            InheritedDataclass(
+                username='test',
+                password='secret',
+                phone='88005553535',
+                email='sensitive@info.com',
+                bank_card='bank_card_info',
+                comment='test comment',
+            ),
+            "InheritedDataclass(username='test', phone='88005553535', comment='test comment')",
+        ),
+    ]
+
+
+@pytest.mark.parametrize(
+    ('dataclass', 'expected_repr'),
+    get_parametrization()
+)
+def test_repr_with_sensitive_data(dataclass, expected_repr):
+    assert repr(dataclass) == expected_repr
+    assert dataclass.password == 'secret'
+    assert dataclass.email == 'sensitive@info.com'
+
+
+@pytest.mark.parametrize(
+    ('dataclass', 'expected_str'),
+    get_parametrization()
+)
+def test_str_sensitive_dataclass(dataclass, expected_str):
+    assert str(dataclass) == expected_str
+    assert dataclass.password == 'secret'
+    assert dataclass.email == 'sensitive@info.com'
