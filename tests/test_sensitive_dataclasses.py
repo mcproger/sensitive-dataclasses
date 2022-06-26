1e91ede59e6216b223774d8a5bbdb5498480cf60
mcproger <mcproger7@gmail.com>
2022-06-26 09:30:50 +0300

    Test case with frozen dataclass

--- tests/test_sensitive_dataclasses.py
+++ tests/test_sensitive_dataclasses.py
@@ -1,74 +1,48 @@
 import pytest
-import dataclasses
-
-from sensitive_dataclasses import sensitive_dataclass, SensitiveData
 
 
-@sensitive_dataclass
-class TestClass:
-    username: str
-    password: SensitiveData
-    phone: str
-    email: SensitiveData
+def test_repr_sensitive_dataclass(usual_dataclass):
+    result = repr(usual_dataclass)
+
+    assert result == "UsualDataclass(username='test', phone='88005553535')"
+    assert usual_dataclass.password == 'secret'
+    assert usual_dataclass.email == 'sensitive@info.com'
 
 
-@sensitive_dataclass
-class TestClassWithExplicitFields:
-    username: str
-    password: SensitiveData = dataclasses.field(
-        default='default-password',
-    )
+def test_class_with_explicit_defined_fields(dataclass_with_explicitly_defined_fields):
+    result = repr(dataclass_with_explicitly_defined_fields)
+
+    assert result == "DataclassWithExplicitlyDefinedFields(username='test')"
+    assert dataclass_with_explicitly_defined_fields.password == 'default-password'
 
 
-@pytest.fixture()
-def test_dataclass():
-    return TestClass(
-        username='test',
-        password='secret',
-        phone='88005553535',
-        email='sensitive@info.com',
-    )
+def test_frozen_dataclass_respectets_sensitive_type(frozen_dataclass):
+    result = repr(frozen_dataclass)
+
+    assert result == "FrozenDataclass(username='test')"
+    assert frozen_dataclass.password == 'top-secret'
 
 
-@pytest.fixture()
-def test_class_with_explicit_fields():
-    return TestClassWithExplicitFields(
-        username='test',
-    )
-
-
-def test_repr_sensitive_dataclass(test_dataclass):
-    result = repr(test_dataclass)
-
-    assert result == "TestClass(username='test', phone='88005553535')"
-
-
-def test_class_with_explicit_defined_fields(test_class_with_explicit_fields):
-    result = repr(test_class_with_explicit_fields)
-
-    assert result == "TestClassWithExplicitFields(username='test')"
-    assert test_class_with_explicit_fields.password == 'default-password'
-
-
-def test_sensitive_dataclass_with_decorator_arguments():
-    pass
-
-
+@pytest.mark.skip
 def test_str_sensitive_dataclass():
     pass
 
 
+@pytest.mark.skip
 def test_sensitive_dataclass_traceback():
     pass
 
 
+@pytest.mark.skip
 def test_log_sensitive_dataclass():
     pass
 
 
+@pytest.mark.skip
 def test_class_inheritance():
     pass
 
 
+@pytest.mark.skip
 def test_sensitive_dataclass_with_no_annotations():
     pass
